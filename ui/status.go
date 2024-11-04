c16080f4e373d48efc259814d32118720f4a4318
Michael Feher <michael.feher@algorand.foundation>
2024-11-04 15:23:05 -0500

    feat: disable ui elements while syncing

--- ui/status.go
+++ ui/status.go
@@ -69,13 +69,21 @@ func (m StatusViewModel) View() string {
 	// Last Round
 	row1 := lipgloss.JoinHorizontal(lipgloss.Left, beginning, middle, end)
 
-	beginning = style.Blue.Render(" Round time: ") + fmt.Sprintf("%.2fs", float64(m.Data.Metrics.RoundTime)/float64(time.Second))
+	roundTime := fmt.Sprintf("%.2fs", float64(m.Data.Metrics.RoundTime)/float64(time.Second))
+	if m.Data.Status.State == "SYNCING" {
+		roundTime = "--"
+	}
+	beginning = style.Blue.Render(" Round time: ") + roundTime
 	end = fmt.Sprintf("%d KB/s ", m.Data.Metrics.TX/1024) + style.Green.Render("TX ")
 	middle = strings.Repeat(" ", max(0, size-(lipgloss.Width(beginning)+lipgloss.Width(end)+2)))
 
 	row2 := lipgloss.JoinHorizontal(lipgloss.Left, beginning, middle, end)
 
-	beginning = style.Blue.Render(" TPS: ") + fmt.Sprintf("%.2f", m.Data.Metrics.TPS)
+	tps := fmt.Sprintf("%.2f", m.Data.Metrics.TPS)
+	if m.Data.Status.State == "SYNCING" {
+		tps = "--"
+	}
+	beginning = style.Blue.Render(" TPS: ") + tps
 	end = fmt.Sprintf("%d KB/s ", m.Data.Metrics.RX/1024) + style.Green.Render("RX ")
 	middle = strings.Repeat(" ", max(0, size-(lipgloss.Width(beginning)+lipgloss.Width(end)+2)))
 
