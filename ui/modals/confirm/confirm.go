e3f2e8f602d7f48824d2498ae63992f74bab7e5d
Michael Feher <michael.feher@algorand.foundation>
2024-11-13 18:18:16 -0500

    chore: move account address in confirm modal

--- ui/modals/confirm/confirm.go
+++ ui/modals/confirm/confirm.go
@@ -1,7 +1,6 @@
 package confirm
 
 import (
-	"fmt"
 	"github.com/algorandfoundation/hack-tui/api"
 	"github.com/algorandfoundation/hack-tui/internal"
 	"github.com/algorandfoundation/hack-tui/ui/app"
@@ -68,13 +67,11 @@ func (m ViewModel) View() string {
 }
 
 func renderDeleteConfirmationModal(partKey *api.ParticipationKey) string {
-	modalStyle := lipgloss.NewStyle().
-		Width(60).
-		Height(7).
-		Align(lipgloss.Center).
-		Padding(1, 2)
-
-	modalContent := fmt.Sprintf("Participation Key: %v\nAccount Address: %v", partKey.Id, partKey.Address)
-
-	return modalStyle.Render("Are you sure you want to delete this key from your node?\n" + modalContent)
+	return lipgloss.NewStyle().Padding(1).Render(lipgloss.JoinVertical(lipgloss.Center,
+		"Are you sure you want to delete this key from your node?\n",
+		style.Cyan.Render("Account Address:"),
+		partKey.Address+"\n",
+		style.Cyan.Render("Participation Key:"),
+		partKey.Id,
+	))
 }
