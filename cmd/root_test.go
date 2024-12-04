6ac6c3e66be3391d081ff5ee838dec980fbb89d0
Tasos Bitsios <tasosbitsios@algorandfndation>
2024-12-04 18:26:26 +0100

    fix test

--- cmd/root_test.go
+++ cmd/root_test.go
@@ -8,7 +8,7 @@ import (
 
 func clearViper() {
 	viper.Set("algod-token", "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
-	viper.Set("algod", "http://localhost:8080")
+	viper.Set("algod-endpoint", "http://localhost:8080")
 	viper.Set("ALGORAND_DATA", "")
 }
 
@@ -26,11 +26,11 @@ func Test_ExecuteRootCommand(t *testing.T) {
 	t.Run("InitConfig", func(t *testing.T) {
 		cwd, _ := os.Getwd()
 		viper.Set("algod-token", "")
-		viper.Set("algod", "")
+		viper.Set("algod-endpoint", "")
 		t.Setenv("ALGORAND_DATA", cwd+"/testdata/Test_InitConfig")
 
 		initConfig()
-		algod := viper.Get("algod")
+		algod := viper.Get("algod-endpoint")
 		if algod == "" {
 			t.Fatal("Invalid Algod")
 		}
@@ -43,11 +43,11 @@ func Test_ExecuteRootCommand(t *testing.T) {
 	t.Run("InitConfigWithoutEndpoint", func(t *testing.T) {
 		cwd, _ := os.Getwd()
 		viper.Set("algod-token", "")
-		viper.Set("algod", "")
+		viper.Set("algod-endpoint", "")
 		t.Setenv("ALGORAND_DATA", cwd+"/testdata/Test_InitConfigWithoutEndpoint")
 
 		initConfig()
-		algod := viper.Get("algod")
+		algod := viper.Get("algod-endpoint")
 		if algod == "" {
 			t.Fatal("Invalid Algod")
 		}
@@ -60,11 +60,11 @@ func Test_ExecuteRootCommand(t *testing.T) {
 	t.Run("InitConfigWithAddress", func(t *testing.T) {
 		cwd, _ := os.Getwd()
 		viper.Set("algod-token", "")
-		viper.Set("algod", "")
+		viper.Set("algod-endpoint", "")
 		t.Setenv("ALGORAND_DATA", cwd+"/testdata/Test_InitConfigWithAddress")
 
 		initConfig()
-		algod := viper.Get("algod")
+		algod := viper.Get("algod-endpoint")
 		if algod == "" {
 			t.Fatal("Invalid Algod")
 		}
@@ -77,11 +77,11 @@ func Test_ExecuteRootCommand(t *testing.T) {
 	t.Run("InitConfigWithAddressAndDefaultPort", func(t *testing.T) {
 		cwd, _ := os.Getwd()
 		viper.Set("algod-token", "")
-		viper.Set("algod", "")
+		viper.Set("algod-endpoint", "")
 		t.Setenv("ALGORAND_DATA", cwd+"/testdata/Test_InitConfigWithAddressAndDefaultPort")
 
 		initConfig()
-		algod := viper.Get("algod")
+		algod := viper.Get("algod-endpoint")
 		if algod == "" {
 			t.Fatal("Invalid Algod")
 		}
