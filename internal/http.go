a917fb81d6005f58ba8317828f8856a15ec3d091
Michael Feher <michael.feher@algorand.foundation>
2024-12-16 10:55:46 -0500

    feat: short link endpoints

--- internal/http.go
+++ internal/http.go
@@ -1,6 +1,9 @@
 package internal
 
-import "net/http"
+import (
+	"io"
+	"net/http"
+)
 
 type HttpPkg struct {
 	HttpPkgInterface
@@ -9,9 +12,13 @@ type HttpPkg struct {
 func (HttpPkg) Get(url string) (resp *http.Response, err error) {
 	return http.Get(url)
 }
+func (HttpPkg) Post(url string, contentType string, body io.Reader) (resp *http.Response, err error) {
+	return http.Post(url, contentType, body)
+}
 
 var Http HttpPkg
 
 type HttpPkgInterface interface {
 	Get(url string) (resp *http.Response, err error)
+	Post(url string, contentType string, body io.Reader) (resp *http.Response, err error)
 }
